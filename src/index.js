e9401f247a4e43fe04dd8043036c10b9413e7e28
haekyu.cho <haekyu.cho@nhn.com>
2022-12-13 22:29:58 +0900

    mydot

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import $ from "jquery";
-import "./index.css";
+import $ from 'jquery';
+import './index.css';
 
-$("#sec").children().children().last().remove();
+$('#sec').children().children().last().remove();
 
 function a(a, b, c) {
   return a + b;
 }
 
-a;
+a(a, s, d, t);
+
+document.querySelector();
